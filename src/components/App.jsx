c2359216273bdac54dd0bcd0ec1f3ad0072ce173
Klaudia Paniuk <kpaniuk@gmail.com>
2023-06-18 20:41:48 +0200

    hw03

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -55,6 +55,20 @@ export class App extends Component {
     );
   };
 
+  componentDidMount = () => {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  };
+
+  componentDidUpdate = (_, prev) => {
+    if (this.state.contacts !== prev.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
   render() {
     const { filter } = this.state;
     const { addContact, deleteContact, typeInputContacts } = this;
